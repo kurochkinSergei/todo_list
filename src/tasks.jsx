0847fc67138059b9629da71f4e9f1b64d64d3726
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2018-08-04 20:57:21 +0300

    task as separate component, show task controls on hover, flip-move

--- src/tasks.jsx
+++ src/tasks.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Button from './button'
+import FlipMove from 'react-flip-move'
 
 class Tasks extends Component {
     constructor (props) {
         super(props)
-        
-    this.createTask = this.createTask.bind(this)    
+
+        this.createTask = this.createTask.bind(this)
+        this.delete = this.delete.bind(this)
     }
 
     delete(key) {
@@ -13,23 +15,81 @@ class Tasks extends Component {
     }
 
     createTask(task) {
-        return <div className="task" key = {task.key}>
-                <Button className="button button_blue" char="9998"/>
-                    {task.title}
-                <Button className="button button_green" char="10003"/>
-                <Button className="button button_red" char="10005" onClick={() => this.delete(task.key)}/>
-            </div>
+        return <Task key={task.key} 
+                     keyProp={task.key} 
+                     title={task.title}
+                     delete={this.delete}/>
+        // <div className="task" 
+        //             key = {task.key}
+        //             onMouseEnter={this.handleMouseHover}
+        //             onMouseLeave={this.handleMouseHover}>
+                
+        //         <Button className="button button_blue" char="9998"/>
+        //             {task.title}
+        //         <div className="task-controls">    
+        //             <Button className="button button_green" char="10003"/>
+        //             <Button className="button button_red" char="10005"                onClick={() => this.delete(task.key)}/>
+        //         </div>
+        //     </div>
     }
+
     render() {
         var tasksEntries = this.props.entries
         var tasks = tasksEntries.map(this.createTask)
 
         return (
             <div className="tasks">
-                {tasks}
+                <FlipMove duration={300} easing="ease-in-out">
+                    {tasks}
+                </FlipMove>    
             </div>    
         )
     }
 }
+
+class Task extends Component {
+    constructor (props) {
+        super(props)
+    
+        this.state = {
+            isHovering: false,
+        };
+
+        this.handleMouseHover = this.handleMouseHover.bind(this);    
+    }
+    
+    handleMouseHover() {
+        this.setState(this.toggleHoverState);
+    }
+
+    toggleHoverState(state) {
+        return {
+          isHovering: !state.isHovering,
+        };
+    }
+
+    render() {
+        return (
+            <div className="task" 
+                    key = {this.props.key}
+                    onMouseEnter={this.handleMouseHover}
+                    onMouseLeave={this.handleMouseHover}>
+                
+                {this.state.isHovering &&
+                    <Button className="button button_blue" char="9998"/>
+                }
+                    {this.props.title}
+                
+                {this.state.isHovering &&
+                    <div className="task-controls">    
+                        <Button className="button button_green" char="10003"/>
+                        <Button className="button button_red" char="10005"                onClick={() => this.props.delete(this.props.keyProp)}/>
+                    </div>
+                }    
+            </div>
+        )
+    }
+}
+
 
 export default Tasks
