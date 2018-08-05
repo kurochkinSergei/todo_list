c74827c726f2295a8ba43d7163d7619fc48562cb
Sergei Kurochkin <kurochkinsergei95@gmail.com>
2018-08-05 12:27:40 +0300

    Task moved to task.jsx

--- src/tasks.jsx
+++ src/tasks.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Button from './button'
+import Task from './task'
 import FlipMove from 'react-flip-move'
 
 class Tasks extends Component {
@@ -31,50 +31,6 @@ class Tasks extends Component {
                     {tasks}
                 </FlipMove>    
             </div>    
-        )
-    }
-}
-
-class Task extends Component {
-    constructor (props) {
-        super(props)
-    
-        this.state = {
-            isHovering: false,
-        };
-
-        this.handleMouseHover = this.handleMouseHover.bind(this);    
-    }
-    
-    handleMouseHover() {
-        this.setState(this.toggleHoverState);
-    }
-
-    toggleHoverState(state) {
-        return {
-          isHovering: !state.isHovering,
-        };
-    }
-
-    render() {
-        return (
-            <div className="task" 
-                    key = {this.props.key}
-                    onMouseOver={this.handleMouseHover}
-                    onMouseLeave={this.handleMouseHover}>
-                
-                {/* {this.state.isHovering && */}
-                    <Button className="button button_blue" char="9998"/>
-                {/* } */}
-                    {this.props.title}
-                
-                {/* {this.state.isHovering && */}
-                    <div className="task-controls">    
-                        <Button className="button button_green" char="10003"/>
-                        <Button className="button button_red" char="10005"                onClick={() => this.props.delete(this.props.keyProp)}/>
-                    </div>
-                {/* }     */}
-            </div>
         )
     }
 }
